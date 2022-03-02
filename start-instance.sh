f3fdc03a68039a00edd62757d2c1eda226f646fc
0xvpr <malikrb@live.com>
2022-03-02 17:44:59 +0000

    start-instance script improved to support IPV6 options

--- start-instance.sh
+++ start-instance.sh
@@ -2,9 +2,21 @@
 
 # Creator:    VPR
 # Created:    February 9th, 2022
-# Updated:    February 9th, 2022
+# Updated:    March 1st, 2022
 
-__docker_img="utopia-dev"
+ENV_FILE="environment.env"
+DOCKER_IMAGE="utopia-dev"
 
-# Mounts local current working directory to /home/utopia in the container
-docker run -ditp 8080:8080 "${__docker_img}" /home/utopia/server/Bin/server.exe
+ENV_PORT=8080  # Port to be exposed and bound to by server
+ENV_IPV6=0     # Set this to 1 to enable IPV6
+
+if [ ${ENV_IPV6} -eq 1 ]:
+then
+    echo "Launching with IPV6 enabled"
+    # Mounts local current working directory to /home/utopia in the container
+    docker run -ditp "${ENV_PORT}:${ENV_PORT}" --ipv6 --env-file ${ENV_FILE} --env "PORT=${ENV_PORT}" --env "IPV6=${ENV_IPV6}" "${DOCKER_IMAGE}" /home/utopia/server/Bin/server.exe
+else
+    echo "Launching with IPV4 enabled"
+    # Mounts local current working directory to /home/utopia in the container
+    docker run -ditp "${ENV_PORT}:${ENV_PORT}" --env-file ${ENV_FILE} --env "PORT=${ENV_PORT}" --env "IPV6=${ENV_IPV6}" "${DOCKER_IMAGE}" /home/utopia/server/Bin/server.exe
+fi
